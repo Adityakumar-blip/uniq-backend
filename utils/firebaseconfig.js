e812ee98fb8c65e8314e069a1e85da4b75d1a325
unsolved-solutions <146564388+unsolved-solutions@users.noreply.github.com>
2024-07-22 23:01:05 +0530

    new changers

--- utils/firebaseconfig.js
+++ utils/firebaseconfig.js
@@ -9,20 +9,3 @@ admin.initializeApp({
 const storage = admin.storage();
 
 module.exports = { storage };
-
-// const { initializeApp } = require("firebase-admin/app");
-// const { getStorage } = require("firebase-admin/storage");
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCymOf3yRPp643UTh4HNTWW1mARFdzsnUE",
-//   authDomain: "dlabss.firebaseapp.com",
-//   projectId: "dlabss",
-//   storageBucket: "dlabss.appspot.com",
-//   messagingSenderId: "625798268669",
-//   appId: "1:625798268669:web:c93bc04b0c1776a66c6094",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const storage = getStorage(app);
-
-// module.exports = { app, storage };
